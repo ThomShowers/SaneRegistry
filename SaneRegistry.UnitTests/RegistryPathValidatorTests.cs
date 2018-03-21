a416d022042cd3341aa195660babf024940258a0
sl4gr00m <sl4gr00m@gmail.com>
2018-03-21 19:37:02 -0300

    Implement registry path validation.
    - Creates the SaneRegistry project and test project.
    
    - Creates a type for performing registry path and name validations.
    
    - Implements a method for validating absolute registry paths.

---
+++ SaneRegistry.UnitTests/RegistryPathValidatorTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SaneRegistry.UnitTests
+{
+    [TestClass]
+    public class RegistryPathValidatorTests
+    {
+        private const string _validBaseKeyName = "HKEY_CURRENT_USER";
+        private const string _invalidBaseKeyName = "HKEY_I_HOPE_THIS_IS_NEVER_A_REGISTRY_KEY_NAME";
+
+        [TestMethod]
+        public void ValidateRegistryKeyPath_NullPath_Throws()
+        {
+            var ex = 
+                Assert.ThrowsException<ArgumentNullException>(
+                    () => RegistryPathValidator.ValidateKeyPath(null));
+            Assert.AreEqual("path", ex.ParamName);
+        }
+
+        [TestMethod]
+        public void ValidateRegistryKeyPath_InvalidBaseKeyName_Throws()
+        {
+            var ex =
+                Assert.ThrowsException<ArgumentException>(
+                    () => RegistryPathValidator.ValidateKeyPath(_invalidBaseKeyName));
+            Assert.AreEqual("path", ex.ParamName);
+        }
+
+        [DataTestMethod]
+        [DataRow("HKEY_CLASSES_ROOT")]
+        [DataRow("HKEY_CURRENT_CONFIG")]
+        [DataRow("HKEY_CURRENT_USER")]
+        [DataRow("HKEY_DYN_DATA")]
+        [DataRow("HKEY_LOCAL_MACHINE")]
+        [DataRow("HKEY_PERFORMANCE_DATA")]
+        [DataRow("HKEY_USERS")]
+        public void ValidateRegistryKeyPath_ValidBaseKeyName_DoesNotThrow(string path)
+        {
+            RegistryPathValidator.ValidateKeyPath(path);
+        }
+
+        [TestMethod]
+        public void ValidateRegistryKeyPath_LowercaseValidBaseKeyName_DoesNotThrow()
+        {
+            RegistryPathValidator.ValidateKeyPath(_validBaseKeyName.ToLower());
+        }
+
+        [TestMethod]
+        public void ValidateRegistryKeyPath_MoreThan510Levels_Throws()
+        {
+            var levels = 511;
+            var keyPath =
+                $@"{_validBaseKeyName}\{string.Join(@"\", Enumerable.Repeat('a', levels - 1))}";
+            var ex =
+                Assert.ThrowsException<ArgumentException>(
+                    () => RegistryPathValidator.ValidateKeyPath(keyPath));
+            Assert.AreEqual("path", ex.ParamName);
+        }
+
+        [TestMethod]
+        public void ValidateRegistryKeyPath_510Levels_DoesNotThrow()
+        {
+            var levels = 510;
+            var keyPath =
+                $@"{_validBaseKeyName}\{string.Join(@"\", Enumerable.Repeat('a', levels - 1))}";
+            RegistryPathValidator.ValidateKeyPath(keyPath);
+        }
+
+        [TestMethod]
+        public void ValidateRegistryKeyPath_PathWithNameLongerThan255_Throws()
+        {
+            var path = $@"{_validBaseKeyName}\{new string(Enumerable.Repeat('a', 256).ToArray())}";
+            var ex =
+                Assert.ThrowsException<ArgumentException>(
+                    () => RegistryPathValidator.ValidateKeyPath(path));
+            Assert.AreEqual("path", ex.ParamName);
+        }
+
+        [TestMethod]
+        public void ValidateRegistryKeyPath_PathWith255CharName_DoesNotThrow()
+        {
+            var path = $@"{_validBaseKeyName}\{new string(Enumerable.Repeat('a', 255).ToArray())}";
+            RegistryPathValidator.ValidateKeyPath(path);
+        }
+
+        [TestMethod]
+        public void ValidateRegistryKeyPath_MoreThan32724CharacterPath_Throws()
+        {
+            var totalLength = 32725;
+            var path = BuildRegistryPath(_validBaseKeyName, totalLength);
+            var ex =
+                Assert.ThrowsException<ArgumentException>(
+                    () => RegistryPathValidator.ValidateKeyPath(path));
+            Assert.AreEqual("path", ex.ParamName);
+        }
+
+        [TestMethod]
+        public void ValidateRegistryKeyPath_32724CharacterPath_DoesNotThrow()
+        {
+            var totalLength = 32724;
+            var path = BuildRegistryPath(_validBaseKeyName, totalLength);
+            RegistryPathValidator.ValidateKeyPath(path);
+        }
+
+        private string BuildRegistryPath(string baseKey, int totalLength)
+        {
+            var path = baseKey + "\\";
+            while (path.Length + 256 < totalLength)
+                path += new string(Enumerable.Repeat('a', 255).ToArray()) + @"\";
+            var lastPathLength = totalLength - path.Length;
+            return path + new string(Enumerable.Repeat('a', lastPathLength).ToArray());
+        }
+    }
+}
---
+++ SaneRegistry/RegistryPathValidator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Win32;
+
+namespace SaneRegistry
+{
+    /// <summary>
+    /// Exposes the validation methods used on registry paths and names.
+    /// </summary>
+    public static class RegistryPathValidator
+    {
+        // This type is loosely based on the specs provided at 
+        // https://msdn.microsoft.com/en-us/library/windows/desktop/ms724872(v=vs.85).aspx. These
+        // specifications provided the types of limitations to expect, but testing revealed that
+        // the values specified were in fact incorrect.
+        
+        private static readonly int _maxTreeDepth = 510;
+        private static readonly int _maxPathLength = 32724;
+        private static readonly int _maxNameLength = 255;
+        private static readonly string[] _baseNames;
+
+        static RegistryPathValidator()
+        {
+            _baseNames =
+                Enum.GetValues(typeof(RegistryHive))
+                .Cast<RegistryHive>()
+                .Select(hKey => RegistryKey.OpenBaseKey(hKey, RegistryView.Default))
+                .Select(baseKey => baseKey.Name)
+                .ToArray();
+        }
+
+        /// <summary>
+        /// Validates absolute registry key paths.
+        /// </summary>
+        /// <param name="path">The path to validate.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="path"/> is <c>null</c>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="path"/> begins with an invalid base key name
+        /// <para/>- or -<para/>
+        /// <paramref name="path"/> contains more than 510 names
+        /// <para/>- or -<para/>
+        /// <paramref name="path"/> contains names that exceed 255 characters
+        /// <para/>- or -<para/>
+        /// <paramref name="path"/> is longer than 32724 characters
+        /// </exception>
+        public static void ValidateKeyPath(string path)
+        {
+            if (path == null) throw new ArgumentNullException(nameof(path));
+
+            var names = path.Split('\\');
+            if (!_baseNames.Contains(names[0].ToUpper()))
+                throw new ArgumentException($"Invalid base key name '{names[0]}'.", nameof(path));
+
+            if (names.Count() > _maxTreeDepth)
+                throw new ArgumentException(
+                    $"Path exceeds maximum tree depth ({_maxTreeDepth}).", nameof(path));
+
+            var overLengthNames = names.Where(n => n.Length > _maxNameLength);
+            if (overLengthNames.Count() > 0)
+                throw new ArgumentException(
+                    $"Path contains a name that exceeds the {_maxNameLength} limit: " +
+                    $"'{overLengthNames.First()}'.",
+                    nameof(path));
+
+            if (path.Length > _maxPathLength)
+                throw new ArgumentException(
+                    $"Path exceeds maximum path length ({_maxPathLength}).", nameof(path));
+        }
+    }
+}
