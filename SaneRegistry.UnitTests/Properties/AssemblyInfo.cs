81ee9b3ca3e472ac932e9aa59bc8e86dee511696
Thomas Showers <showerstm@gmail.com>
2018-03-21 19:37:02 -0300

    Implement registry path validation.
    - Creates the SaneRegistry project and test project.
    
    - Creates a type for performing registry path and name validations.
    
    - Implements a method for validating absolute registry paths.

---
+++ SaneRegistry.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SaneRegistryUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SaneRegistryUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("251292a0-2c95-47b2-91b4-3d990487fcc9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
